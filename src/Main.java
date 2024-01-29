c567fbc7993ded7f031e3fc0c5f2b09eb2ad7dc6
Quoc Khanh Nguyen <nguyen5@SPSEJECNANET.CZ>
2024-01-29 10:42:08 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Map map = new Map("rule.txt");
+        System.out.println(map.getRooms());
+
+
+
+    }
+}
---
+++ src/Map.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Map {
+    private HashMap<Integer,String> rooms=  new HashMap<>();
+
+
+    public Map(String file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line=br.readLine())!=null){
+                int roomID = Integer.parseInt(line.substring(0,1));
+                String roomName = line.substring(10);
+                rooms.put(roomID,roomName);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public HashMap<Integer, String> getRooms() {
+        return rooms;
+    }
+}
